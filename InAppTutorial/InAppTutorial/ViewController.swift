08ba1a15c5b2600f8b51bd6c373c58d8e0cb9573
SMalagic <serkanmalagic@hotmail.com>
2021-02-16 17:17:26 +0300

    - New pod added

--- InAppTutorial/InAppTutorial/ViewController.swift
+++ InAppTutorial/InAppTutorial/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import SwiftyStoreKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getProducts()
     }
 
+    func getProducts(){
+        
+        SwiftyStoreKit.retrieveProductsInfo( [
+            "com.derspektif.DPD.00001","com.derspektif.DPD.00002","com.derspektif.DPD.00003","com.derspektif.DPD.00004","com.derspektif.DPD.00006","com.derspektif.DPD.00007","com.derspektif.DPD.00008"
+        ] ) { result in
+            
+            
+            for item in result.retrievedProducts {
+//                print("Ürün Adı : \(item.localizedTitle)")
+//                print("Ürün Fiyatı : \(item.price)")
+                print(item.localizedTitle)
+            }
+            
+            if let product = result.retrievedProducts.first {
+                let priceString = product.localizedPrice!
+                print("Product: \(product.localizedDescription), price: \(priceString)")
+            }
+            else if let invalidProductId = result.invalidProductIDs.first {
+                print("Invalid product identifier: \(invalidProductId)")
+            }
+            else {
+                print("Error: \(result.error)")
+            }
+        }
+        
+    }
 
 }
 
